35e3cb6f2158977847d1f01c16346e95cdf2f997
Biswajit Basak <juelbasak@gmail.com>
2021-08-19 01:52:27 +0530

    Debugging

--- main.py
+++ main.py
@@ -61,6 +61,48 @@ full_pipeline = ColumnTransformer([
 ])
 
 
+
+
+def fetch_data(housing_url, housing_path):
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, 'housing.tgz')
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+def load_data(housing_path):
+    csv_path = os.path.join(housing_path, 'housing.csv')
+    return pd.read_csv(csv_path)
+
+
+def model_process():
+    DOWNLOAD_ROOT = 'https://raw.githubusercontent.com/ageron/handson-ml2/master/'
+    HOUSING_PATH = os.path.join('datasets/', 'housing')
+    HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + '/housing.tgz'
+
+    if not os.path.exists('./datasets/housing/housing.csv'):
+        fetch_data(HOUSING_URL, HOUSING_PATH)
+
+    housing = load_data(HOUSING_PATH)
+
+    housing['income_cat'] = pd.cut(housing['median_income'], bins=[0, 1.5, 3.0, 4.5, 6, np.inf], labels=[1, 2, 3, 4, 5])
+
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+    for train_index, test_index in split.split(housing, housing['income_cat']):
+        strat_train_set = housing.loc[train_index]
+        strat_test_set = housing.loc[test_index]
+
+    strat_train_set.drop('income_cat', axis=1, inplace=True)
+    strat_test_set.drop('income_cat', axis=1, inplace=True)
+
+    housing = strat_train_set.copy()
+
+    full_pipeline.fit_transform(housing.drop('median_house_value', axis=1))
+
+
 @app.route('/', methods=['GET'])
 def homepage():
     return render_template('index.html')
@@ -121,7 +163,7 @@ def model_predict():
     new_data = [
         [longitude, latitude, housing_median_age, total_rooms, total_bedrooms, population, households, median_income,
          ocean_proximity]]
-    print(new_data)
+    # print(new_data)
     new_df = pd.DataFrame(new_data, columns=num_attribs + cat_attribs)
 
     try:
@@ -136,47 +178,7 @@ def model_predict():
         return render_template('index.html', flag=True)
     return render_template('index.html', predicted_value=predicted_value[0], flag=False)
 
-
-def fetch_data(housing_url, housing_path):
-    os.makedirs(housing_path, exist_ok=True)
-    tgz_path = os.path.join(housing_path, 'housing.tgz')
-    urllib.request.urlretrieve(housing_url, tgz_path)
-    housing_tgz = tarfile.open(tgz_path)
-    housing_tgz.extractall(path=housing_path)
-    housing_tgz.close()
-
-
-def load_data(housing_path):
-    csv_path = os.path.join(housing_path, 'housing.csv')
-    return pd.read_csv(csv_path)
-
-
-def model_process():
-    DOWNLOAD_ROOT = 'https://raw.githubusercontent.com/ageron/handson-ml2/master/'
-    HOUSING_PATH = os.path.join('datasets/', 'housing')
-    HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + '/housing.tgz'
-
-    if not os.path.exists('./datasets/housing/housing.csv'):
-        fetch_data(HOUSING_URL, HOUSING_PATH)
-
-    housing = load_data(HOUSING_PATH)
-
-    housing['income_cat'] = pd.cut(housing['median_income'], bins=[0, 1.5, 3.0, 4.5, 6, np.inf], labels=[1, 2, 3, 4, 5])
-
-    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-
-    for train_index, test_index in split.split(housing, housing['income_cat']):
-        strat_train_set = housing.loc[train_index]
-        strat_test_set = housing.loc[test_index]
-
-    strat_train_set.drop('income_cat', axis=1, inplace=True)
-    strat_test_set.drop('income_cat', axis=1, inplace=True)
-
-    housing = strat_train_set.copy()
-
-    full_pipeline.fit_transform(housing.drop('median_house_value', axis=1))
-
-
+    
 if __name__ == '__main__':
     model_process()
     app.run(debug=False)
